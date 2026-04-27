7e308f6bf0a3cfb7ace14e5133484833b1c1c6b6
ZhenisM <zhenisbtr@gmail.com>
2026-04-27 13:57:52 +0500

    add new component - htmlUnescape

--- lib/catalog/product_list/widgets/product_tile.dart
+++ lib/catalog/product_list/widgets/product_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 import 'package:andasia/repositories/products/models/product.dart';
 import 'package:andasia/router/router.dart';
+import 'package:html_unescape/html_unescape.dart';
 
 
 class ProductTile extends StatelessWidget {
@@ -12,19 +13,16 @@ class ProductTile extends StatelessWidget {
 
   final Product product;
 
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ListTile(
       //leading: Image.network(product.imageUrl),
       title: Text(
-        product.name,
+        HtmlUnescape().convert(product.name),
         style: theme.textTheme.bodyMedium,
       ),
-      /*subtitle: Text(
-        '${product.price} \$',
-        style: theme.textTheme.labelSmall,
-      ),*/
       trailing: const Icon(Icons.arrow_forward_ios),
       onTap: () {
         Navigator.of(context).pushNamed(
